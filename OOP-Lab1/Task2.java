e29db924b0dcc96378176eaab85f012d1cff440b
Raja Aakash <49368333+pro44@users.noreply.github.com>
2019-04-07 20:27:28 +0500

    Add files via upload

---
+++ OOP-Lab1/Task2.java
@@ -0,0 +1,10 @@
+class Task2{
+public static void main(String args[]){
+double a = 50.25;
+long b = (int)50.25;
+double c = a%b;
+System.out.println("Double(actual)	= "+a);
+System.out.println("Integer		= "+b);
+System.out.println("Fractional	= "+c);
+}
+}
