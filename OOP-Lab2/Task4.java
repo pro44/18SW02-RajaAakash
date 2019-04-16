ee0a6278107b89cca7103a8b935e649d2d6d5a00
Raja Aakash <49368333+pro44@users.noreply.github.com>
2019-04-16 16:43:22 +0500

    Add files via upload

---
+++ OOP-Lab2/Task4.java
@@ -0,0 +1,14 @@
+class Task4{
+public static void main(String args[]){
+	int radius = 4;
+	double diameter = 2*radius;
+	double circumference = 2*3.14*radius;
+	double area = 3.14*radius*radius;
+	System.out.println("Radius: "+radius);
+	System.out.println("Diameter: "+diameter);
+	System.out.println("Circumference: "+circumference);
+	System.out.println("Area: "+area);
+	
+	
+}
+}
